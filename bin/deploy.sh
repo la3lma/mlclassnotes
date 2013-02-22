d01d9b864de9731a8e6697a3ab2f5a109bc320f3
Bjørn Remseth <la3lma@gmail.com>
2013-02-22 23:43:09 +0100

    Improved deployment script.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 SOURCE=notes.pdf
-TARGET=$HOME/Desktop/Dropbox/Public/machine-learning-notes.pdf
+TARGETDIR=/Users/rmz/Dropbox/Public
+TARGET=$TARGETDIR/machine-learning-notes.pdf
+
+if [ ! -d "$TARGETDIR" ] ; then
+   echo "Couldn't find target dir to copy to: $TARGETDIR"
+   exit 1
+fi
 
 if [ -f "$SOURCE" ] ; then
    cp $SOURCE $TARGET
